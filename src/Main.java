421088dc56cc762a0cc39b59dd4273ba51df41d8
eishakhan12 <89078277+eishakhan12@users.noreply.github.com>
2021-10-30 02:16:22 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToe tictactoe = new TicTacToe();
+
+    }
+}
